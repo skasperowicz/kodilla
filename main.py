4fd173f043a706a438b2119ed20a39c2443f23fc
Sławek Kasperowicz <slawek.kasperowicz@gmail.com>
2024-04-19 18:12:50 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+shopping_dict = {
+    "Piekarnia": ['Chleb', 'Pączek', 'Bułki'],
+    "Warzywniak": ['Marchew', 'Seler', 'Rukola'],
+}
+product_sum = 0
+for i in shopping_dict:
+    print(f"Idę do {i} i kupuję tam {shopping_dict[i]}")
+    product_sum = product_sum + len(shopping_dict[i])
+print(f"W sumie kupuję {product_sum} produktów")
